163eec5f95e5c42c65a362a7c35370c36e164f69
osalvatierra <oscar@owebdesigns.com>
2022-07-07 16:11:26 -0400

    testing dist removal

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,9 +8,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
    
 const controlRecipes = async function () {
   try {
