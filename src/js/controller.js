da71b1e2a3677a831cbd59705d5c03155e05c437
osalvatierra <oscar@owebdesigns.com>
2022-07-07 16:56:41 -0400

    finalized bookmark functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -18,6 +19,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search results
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -69,12 +71,17 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+ 
+  // 1. Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else if (model.state.recipe.bookmarked)
     model.deleteBookmark(model.state.recipe.id);
 
-  console.log(model.state.recipe);
+  //2. Update Recipe View
   recipeView.update(model.state.recipe);
+
+  //3. Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
