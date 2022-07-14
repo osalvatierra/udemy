ab4850e70505dfc2153f27cb0b793f7f2ae31c0f
osalvatierra <oscar@owebdesigns.com>
2022-07-14 13:22:09 -0400

    final updates

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,6 +2,16 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
 	_data;
+
+	/**
+	 * Render the received object to the DOM
+	 * @param {Object | Object[]} data The data to be rendered (e.g. rececipe)
+	 * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+	 * @returns {undefined | string } A markup string is return if render = false
+	 * @this {Object} View instance
+	 * @todo Finish implementation
+	 */
+
 	render(data, render = true) {
 		if (!data || (Array.isArray(data) && data.length === 0))
 			return this.renderError();
@@ -9,7 +19,7 @@ export default class View {
 		this._data = data;
 		const markup = this._generateMarkup();
 
-		if(!render) return markup;
+		if (!render) return markup;
 
 		this._clear();
 		this._parentElement.insertAdjacentHTML('afterbegin', markup);
